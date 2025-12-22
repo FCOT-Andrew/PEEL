f1f43f6a87ff22a5f65642e9e572c141c5956255
Andrew <a.lucas@farn-ct.ac.uk>
2025-12-22 12:33:18 +0000

    Tweak the prompt to make links clearer

--- data.ts
+++ data.ts
@@ -1,7 +1,7 @@
 export default {
     f0e4c2f76c58916ec258f246851bea091d14d4247a2fc3e18694461b1816e13b: {
         brief: [
-            "1. Describe one opportunity for a teacher to integrate artificial intelligence (AI) tools into their teaching practice.",
+            "Describe one opportunity for a teacher to integrate artificial intelligence (AI) tools into their teaching practice.",
         ],
         outcomes: [
             "* Use the PEEL structure.",
@@ -10,7 +10,7 @@ export default {
             "  3. Explanation, Example or Evidence",
             "  4. Link",
             "* Write a response of approximately 80-120 words.",
-            "* Successfully join the two domains in the question.",
+            "* Successfully join the two domains in the brief.",
         ],
     }
 } as Record<string, { brief: string[]; outcomes: string[] }>;
